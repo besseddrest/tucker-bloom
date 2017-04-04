d14537e5548a518dc5bbf497f5944d4da5cadd79
besseddrest <hcabalic@gmail.com>
2017-04-04 11:18:44 -0700

    add script for swatches, work on thumb responsiveness

--- assets/product-responsive.js
+++ assets/product-responsive.js
@@ -1,5 +1,27 @@
-const desc = document.querySelector('div[data-title="Description"] p:first-child').cloneNode(true);
-/*const cloned = desc.cloneNode();*/
-const descMain = document.querySelector('.product--description');
-console.log(desc);
+/* clones the product description and adds to product main section */
+const desc = document.querySelector('div[data-title="Description"] p:first-child').cloneNode(true),
+      descMain = document.querySelector('.product--description');
 descMain.appendChild(desc);
+
+/* adds user selection to swatch label */
+const swatchGroup = Array.from(document.querySelectorAll('.swatch'));
+swatchGroup.forEach(group => {
+  group.addEventListener('click', (e) => {
+    const el = e.target;
+
+    if (el.tagName == 'INPUT') {
+      const header = group.querySelector('.header'),
+            val = el.value,
+            selection = group.querySelector('.user--selection');
+
+      if (selection) {
+        selection.innerHTML = val;
+      } else {
+        const wrapper = document.createElement('strong');
+        wrapper.classList.add('user--selection');
+        wrapper.innerHTML = val;
+        header.appendChild(wrapper);
+      }
+    }
+  });
+});
