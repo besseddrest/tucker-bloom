ad18a0ba4ef1898657fb732da78355b3823af501
besseddrest <hcabalic@gmail.com>
2017-04-05 16:48:54 -0700

    update CSS

--- assets/product-responsive.js
+++ assets/product-responsive.js
@@ -1,27 +1,29 @@
 /* clones the product description and adds to product main section */
-const desc = document.querySelector('div[data-title="Description"] p:first-child').cloneNode(true),
+const desc = document.querySelector('div[data-title="Description"] > p'),
       descMain = document.querySelector('.product--description');
-descMain.appendChild(desc);
 
-/* adds user selection to swatch label */
+if (desc) {
+  const cloned = desc.cloneNode(true);
+  descMain.appendChild(cloned);
+}
+
 const swatchGroup = Array.from(document.querySelectorAll('.swatch'));
 swatchGroup.forEach(group => {
+  const header = group.querySelector('.header'),
+        firstSwatch = group.querySelector('.swatch-element'),
+        wrapper = document.createElement('strong');
+
+  /* update labels for default options */
+  wrapper.innerHTML = firstSwatch.dataset.value;
+  wrapper.classList.add('user--selection');
+  header.appendChild(wrapper);
+
+  /* adds user selection to swatch label */
   group.addEventListener('click', (e) => {
     const el = e.target;
 
     if (el.tagName == 'INPUT') {
-      const header = group.querySelector('.header'),
-            val = el.value,
-            selection = group.querySelector('.user--selection');
-
-      if (selection) {
-        selection.innerHTML = val;
-      } else {
-        const wrapper = document.createElement('strong');
-        wrapper.classList.add('user--selection');
-        wrapper.innerHTML = val;
-        header.appendChild(wrapper);
-      }
+      wrapper.innerHTML = el.value;
     }
   });
 });
