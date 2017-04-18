6c4817c364e8ad8684acc311116658dccd98b15a
besseddrest <hcabalic@gmail.com>
2017-04-17 20:12:55 -0700

    add logic for tabs

--- assets/product-responsive.js
+++ assets/product-responsive.js
@@ -2,9 +2,44 @@
 const desc = document.querySelector('div[data-title="Description"] > p'),
       descMain = document.querySelector('.product--description');
 
+const tabs = document.querySelector('#product-description'),
+      tabsFull = document.querySelector('.tabs__full'),
+      tabsMobile = document.querySelector('.tabs__mobile');
+
 if (desc) {
   const cloned = desc.cloneNode(true);
   descMain.appendChild(cloned);
+}
+
+const getCurrentWidth = () => {
+  return window.innerWidth || document.body.clientWidth;
+}
+
+const renderTabsFull = () => {
+  tabsFull.appendChild(tabs);
+  tabsMobile.innerHTML = '';
+}
+
+const renderTabsMobile = () => {
+  tabsMobile.appendChild(tabs);
+  tabsFull.innerHTML = '';
+}
+
+const renderInitialTabs = () => {
+  const browserWidth = getCurrentWidth();
+  browserWidth <= 480 ? renderTabsMobile() : renderTabsFull();
+}
+
+renderInitialTabs();
+
+window.onresize = () => {
+  const currentWidth = getCurrentWidth();
+
+  if (currentWidth <= 480 && tabsMobile.innerHTML == '')
+    renderTabsMobile();
+
+  if (currentWidth > 480 && tabsFull.innerHTML == '')
+    renderTabsFull();
 }
 
 const swatchGroup = Array.from(document.querySelectorAll('.swatch'));
